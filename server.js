a7f578f155a39ff16bfc661b78aac111a9cf03ee
himanshup <himanshup@csu.fullerton.edu>
2018-08-28 01:38:17 -0700

    update server file

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
  * This is an example of a basic node.js script that performs
  * the Authorization Code oAuth2 flow to authenticate against
  * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 require("dotenv").config();
 var express = require("express"); // Express web server framework
