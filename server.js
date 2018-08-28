53e007b5f3662e3011a2b098e74f9a0f44b2c38e
himanshup <himanshup@csu.fullerton.edu>
2018-08-28 01:41:00 -0700

    update redirect uri

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ var x = require("x-ray")();
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = "http://localhost:8888/callback"; // Your redirect uri
+var redirect_uri =
+  "http://localhost:8888/callback" ||
+  "https://playlistcreator.herokuapp.com/callback"; // Your redirect uri
 var port = process.env.PORT || 8888;
 /**
  * Generates a random string containing numbers and letters
