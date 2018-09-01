1b70f43dcc230e6d60dacb584ff638369ff67b19
himanshup <himanshup@csu.fullerton.edu>
2018-09-01 16:57:15 -0700

    rename server file

---
+++ app.js
@@ -0,0 +1,227 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Authorization Code oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ */
+require("dotenv").config();
+var express = require("express"); // Express web server framework
+var path = require("path");
+var request = require("request"); // "Request" library
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+var x = require("x-ray")();
+var schedule = require("node-schedule");
+
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+var redirect_uri = "http://localhost:8888/callback";
+// Your redirect uri
+var port = process.env.PORT || 8888;
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var j = schedule.scheduleJob("* */6 * * *", function() {
+  getSongs();
+});
+
+function getSongs() {
+  x("https://www.billboard.com/charts/hot-100", ".chart-number-one", [
+    {
+      rank: "[data-rank]@data-rank",
+      title: ".chart-number-one__title",
+      artist: ".chart-number-one__artist a"
+    }
+  ]).write("./songs/billboard1.json");
+
+  x("https://www.billboard.com/charts/hot-100", ".chart-list-item", [
+    {
+      rank: ".chart-list-item__rank ",
+      title: ".chart-list-item__title-text",
+      artist: ".chart-list-item__artist"
+    }
+  ]).write("./songs/billboard99.json");
+}
+
+var stateKey = "spotify_auth_state";
+
+var app = express();
+
+app.use(cors()).use(cookieParser());
+
+if (process.env.NODE_ENV !== "production") {
+  // var redirect_uri = "https://playlistcreator.herokuapp.com/callback";
+  app.get("/login", function(req, res) {
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    var scope =
+      "user-read-private user-read-email user-read-playback-state playlist-modify-private playlist-modify-public user-top-read";
+    res.redirect(
+      "https://accounts.spotify.com/authorize?" +
+        querystring.stringify({
+          response_type: "code",
+          client_id: client_id,
+          scope: scope,
+          redirect_uri: redirect_uri,
+          state: state
+        })
+    );
+  });
+
+  app.get("/callback", function(req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+      res.redirect(
+        "/#" +
+          querystring.stringify({
+            error: "state_mismatch"
+          })
+      );
+    } else {
+      res.clearCookie(stateKey);
+      var authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: "authorization_code"
+        },
+        headers: {
+          Authorization:
+            "Basic " +
+            new Buffer(client_id + ":" + client_secret).toString("base64")
+        },
+        json: true
+      };
+
+      request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+          var options = {
+            url: "https://api.spotify.com/v1/me",
+            headers: { Authorization: "Bearer " + access_token },
+            json: true
+          };
+
+          // use the access token to access the Spotify Web API
+          request.get(options, function(error, response, body) {
+            // console.log(body);
+          });
+
+          // we can also pass the token to the browser to make requests from there
+
+          res.redirect(
+            "http://localhost:3000/#" +
+              querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token
+              })
+          );
+        } else {
+          res.redirect(
+            "/#" +
+              querystring.stringify({
+                error: "invalid_token"
+              })
+          );
+        }
+      });
+    }
+  });
+
+  app.get("/refresh_token", function(req, res) {
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer(client_id + ":" + client_secret).toString("base64")
+      },
+      form: {
+        grant_type: "refresh_token",
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        res.send({
+          access_token: access_token
+        });
+      }
+    });
+  });
+}
+
+// API calls
+app.get("/api/billboard1", function(req, res) {
+  // var stream = x(
+  //   "https://www.billboard.com/charts/hot-100",
+  //   ".chart-number-one",
+  //   [
+  //     {
+  //       rank: "[data-rank]@data-rank",
+  //       title: ".chart-number-one__title",
+  //       artist: ".chart-number-one__artist a"
+  //     }
+  //   ]
+  // ).stream();
+  // stream.pipe(res);
+
+  res.sendFile(path.join(__dirname, "/songs", "billboard1.json"));
+});
+
+app.get("/api/billboard100", function(req, res) {
+  // var stream = x(
+  //   "https://www.billboard.com/charts/hot-100",
+  //   ".chart-list-item",
+  //   [
+  //     {
+  //       rank: ".chart-list-item__rank ",
+  //       title: ".chart-list-item__title-text",
+  //       artist: ".chart-list-item__artist"
+  //     }
+  //   ]
+  // ).stream();
+  // stream.pipe(res);
+
+  res.sendFile(path.join(__dirname, "/songs", "billboard99.json"));
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
