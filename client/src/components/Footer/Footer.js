7499e3f9cacc867a6b615f91c911b3b41593b638
himanshup <himanshup@csu.fullerton.edu>
2018-09-01 00:22:00 -0700

    replace links with icon

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,28 +1,18 @@
 import React, { Component } from "react";
+import { FaGithub } from "react-icons/fa";
 import "./Footer.css";
 class Footer extends Component {
   render() {
     return (
-      <div className="mt-4 mb-3">
+      <div className="mt-5 mb-4">
         <footer className="footer">
           <div className="container text-center">
-            <small className="text-muted">
-              Created using the{" "}
-              <a
-                href="https://developer.spotify.com/documentation/web-api/"
-                className="footerLinks"
-              >
-                Spotify Web API
-              </a>
-              . Website by{" "}
-              <a
-                className="footerLinks"
-                href="https://github.com/himanshup/spotify-playlist-creator"
-              >
-                himanshup
-              </a>
-              .
-            </small>
+            <a
+              href="https://github.com/himanshup/spotify-playlist-creator"
+              className="text-dark"
+            >
+              <FaGithub className="footerIcon" />
+            </a>
           </div>
         </footer>
       </div>
