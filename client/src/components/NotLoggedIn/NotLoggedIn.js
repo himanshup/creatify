90ed9ce8d3d8d6e1d2d71ef7a942338b68d220f1
himanshup <himanshup@csu.fullerton.edu>
2018-10-15 22:07:20 -0700

    add footer to the page for people who aren't logged in

--- client/src/components/NotLoggedIn/NotLoggedIn.js
+++ client/src/components/NotLoggedIn/NotLoggedIn.js
@@ -1,46 +1,55 @@
 import React from "react";
 import { Container, Jumbotron } from "reactstrap";
+import Footer from "../Footer/Footer";
 import "./NotLoggedIn.css";
 
 const NotLoggedIn = () => {
   return (
-    <Jumbotron className="rounded-0 homeJumbotron">
-      <Container className="bg-transparent">
-        <h1 className="display-4 text-center ">Playlist Creator</h1>
-        <p className="lead text-center">Easily create Spotify playlists.</p>
-        <div className="text-center mt-3">
-          <p className="lead">
-            Create a playlist based on an artist, search criteria, your top
-            tracks/artists, or with the top 100 songs from Billboard. Search for
-            an artist and we will find the top tracks based on that artist. You
-            can also search for keywords and we will find the top 50 matching
-            playlists, along with the top 100 tracks across all 50 playlists.
-          </p>
+    <div>
+      <Jumbotron className="rounded-0 homeJumbotron">
+        <Container className="bg-transparent">
+          <h1 className="display-4 text-center ">Playlist Creator</h1>
+          <p className="lead text-center">Easily create Spotify playlists.</p>
+          <div className="text-center mt-3">
+            <p className="lead">
+              Create a playlist based on an artist, search criteria, your top
+              tracks/artists, or with the top 100 songs from Billboard. Search
+              for an artist and we will find the top tracks based on that
+              artist. You can also search for keywords and we will find the top
+              50 matching playlists, along with the top 100 tracks across all 50
+              playlists.
+            </p>
 
-          <p className="lead">
-            To get started, login with Spotify. If you don't want to login, view
-            pictures{" "}
+            <p className="lead">
+              To get started, login with Spotify. If you don't want to login,
+              view pictures{" "}
+              <a
+                href="https://github.com/himanshup/spotify-playlist-creator"
+                className="customLinks"
+              >
+                here
+              </a>
+              .
+            </p>
             <a
-              href="https://github.com/himanshup/spotify-playlist-creator"
-              className="customLinks"
+              className="btn badge-pill btn-success btn-lg mt-1 shadow"
+              href={
+                window.location.href.includes("localhost")
+                  ? "http://localhost:8888/login"
+                  : "https://playlistcreator-backend.herokuapp.com/login"
+              }
             >
-              here
+              <span className="p-4 text-uppercase btns">
+                Login With Spotify
+              </span>
             </a>
-            .
-          </p>
-          <a
-            className="btn badge-pill btn-success btn-lg mt-1 shadow"
-            href={
-              window.location.href.includes("localhost")
-                ? "http://localhost:8888/login"
-                : "https://playlistcreator-backend.herokuapp.com/login"
-            }
-          >
-            <span className="p-4 text-uppercase btns">Login With Spotify</span>
-          </a>
-        </div>
+          </div>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <Footer />
       </Container>
-    </Jumbotron>
+    </div>
   );
 };
 
